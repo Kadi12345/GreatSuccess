e318e7bad20abe033d463e8ea6859866e0dc837e
Taive Sarnik <taive.sarnik@gmail.com>
2021-01-26 15:10:00 +0200

    Changed schemas and relations between them

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,16 +1,35 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const schema = new Schema({
-    //userID: { type: String, unique: true, required: true, minlength: 1, maxlength: 10, match: [/^[a-zA-Z0-9-.]+$/, 'is invalid'] },
-    userType: {type: String, enum : ['ADMIN','NOT_ADMIN'], required: true},
-    firstName: { type: String, required: true, minlength: 2, maxlength: 15 },
-    lastName: { type: String, required: true, maxlength: 15 },
-    email: { type: String, required: true  },
-    address: { type: String, required: true, street: String, city: String, state: String, zip: Number },
-    createdDate: { type: Date, default: Date.now },
+const usersSchema = new Schema({
+    userType: {
+        type: String, 
+        enum : ['ADMIN','NOT_ADMIN'], 
+        required: true},
+    firstName: { 
+        type: String, 
+        required: true, 
+        minlength: 2, 
+        maxlength: 15 },
+    lastName: { 
+        type: String, 
+        required: true, 
+        maxlength: 15 },
+    email: { 
+        type: String, 
+        required: true  },
+    address: { 
+        type: String, 
+        required: true, 
+        street: String, 
+        city: String, 
+        state: String, 
+        zip: Number },
+    createdDate: { 
+        type: Date, 
+        default: Date.now },
 })
 
 schema.set('toJSON', { virtuals: true })
 
-module.exports = mongoose.model('users', schema)
+module.exports = mongoose.model('users', usersSchema)
