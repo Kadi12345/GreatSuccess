7c764434b1636ef819e104887bf6d7da62646aab
Kaido24 <mumm.meelis@gmail.com>
2021-02-03 13:31:28 +0200

    order invoice calculations and GET for a single product

--- src/controllers/addOrder.controller.js
+++ src/controllers/addOrder.controller.js
@@ -1,12 +1,41 @@
-const db = require('./../db')
-const Orders = db.Orders
+const db = require('./../db');
+const Orders = db.Orders;
+const Users = db.Users;
+const Products = db.Products;
 
 module.exports = async function (req, res) {
   try {
-    await Orders.create(req.body)
-    console.log('Tere!')
-    res.status(200).json({ message: 'Õnnestus!' })
+    const client = await Users.findOne({ _id: req.body.client });
+    console.log(client);
+
+    const order = {
+      orderNumber: req.body.orderNumber,
+      orderStatus: req.body.orderStatus,
+      deliveryMethod: req.body.deliveryMethod,
+      deliveryAddress: req.body.deliveryAddress,
+      parcelMachine: req.body.parcelMachine,
+      client: {
+        clientID: client._id,
+        userType: client.userType,
+        firstName: client.firstName,
+        lastName: client.lastName,
+        email: client.email,
+        address: client.address,
+      },
+      products: [],
+    };
+    for await (const product of req.body.products) {
+      const productFromDb = await Products.findOne({ _id: product.productID });
+      product.price = productFromDb.price;
+      product.productName = productFromDb.productName;
+      order.products.push(product);
+    }
+   
+    console.log(order);
+    await Orders.create(order);
+    console.log('Tere!');
+    res.status(200).json({ message: 'Õnnestus!' });
   } catch (error) {
-    res.status(500).json({ message: error })
+    res.status(500).json({ message: error });
   }
-}
+};
--- src/router.js
+++ src/router.js
@@ -1,21 +1,29 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const { getAllUsers, addUser, updateUser, getAllOrders, addOrder, updateOrder, addProduct, getAllProducts } = require('./controllers')
+const {
+  getAllUsers,
+  addUser,
+  updateUser,
+  getAllOrders,
+  addOrder,
+  updateOrder,
+  addProduct,
+  getAllProducts,
+  getOneProductByID,
+} = require('./controllers');
 
-router.get('/users', getAllUsers)
-router.post('/user', addUser)
-router.patch('/user/:id', updateUser)
+router.get('/users', getAllUsers);
+router.post('/user', addUser);
+router.patch('/user/:id', updateUser);
 
+router.get('/orders', getAllOrders);
+router.post('/order', addOrder);
+router.patch('/order/:id', updateOrder);
 
-router.get('/orders', getAllOrders)
-router.post('/order', addOrder)
-router.patch('/order/:id', updateOrder)
+router.post('/product', addProduct);
+router.get('/products', getAllProducts);
 
+router.get('/product/:id', getOneProductByID);
 
-router.post('/product', addProduct)
-router.get('/products', getAllProducts)
-
-
-
-module.exports = router
+module.exports = router;
